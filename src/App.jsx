151edc8af693a48e583dfe3b46498a1935d85152
Joy-Kendi_del <jkendi083@gmail.com>
2026-09-15 10:43:42 +0300

    fix deployment issue

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import "./App.css"
+import  { useState } from 'react'
+
+import FormSplitBill from "./components/FormSplitBill";
+import Button from "./components/Button";
+import FormAddFriend from "./components/FormAddFriend";
+import FriendsList from "./components/FriendsList";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+
+function App() 
+{
+  const [friends,setFriends] =useState(initialFriends)
+  const [selectedFriend,setSelectedFriend] =useState(null)
+  const [showAddFriend,setShowAddFriend] =useState(false)
+  function handleShowAddFriend(){
+    setShowAddFriend(show=>!show)
+  }
+  function handleAddFriend(friend){
+    setFriends(friends=>[friend,...friends])
+    setShowAddFriend(false)
+  }
+  function handleSelection(friend){
+    setSelectedFriend(cur=>
+      cur?.id ===friend.id ?null :friend
+    )
+    setShowAddFriend(false)
+  }
+  function handleSplitBill(value){
+    setFriends(friends=>
+      friends.map(friend=>
+        friend.id ===selectedFriend.id ? {...friend,balance:friend.balance+value} :friend
+      )
+    )
+setSelectedFriend(null)
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+       <FriendsList friends={friends} selectedFriend={selectedFriend} onSelection={handleSelection}/>
+    {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend}/>}
+        <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" :"Add friend"}</Button>
+      </div>
+
+     {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill}/>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FriendsList.jsx
@@ -0,0 +1,15 @@
+
+import Friend from './Friend'
+
+function FriendsList({friends,selectedFriend,onSelection}) {
+  return (
+     <ul>
+          {friends.map(friend=>(
+            <Friend key={friend.id} friend={friend} selectedFriend={selectedFriend} onSelection={onSelection}/>
+          ))}
+        </ul>
+        
+  )
+}
+
+export default FriendsList
